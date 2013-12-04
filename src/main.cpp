c748fc2493cb2795d6e553aae1477df10729de46
Chris Louie <deadpotat00@yahoo.com>
2013-12-04 00:16:12 -0800

    fixed precompile header makefile.
    reading in a file into a string.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include "precompile.h"
 
+#include <iostream>
+#include <fstream>
+#include <string>
+
 int main(int argc, char* argv[])
 {
+    std::ifstream in("test.js", std::ios::in | std::ios::binary);
+    if(in)
+    {
+        std::string contents;
+        in.seekg(0, std::ios::end);
+        contents.resize(in.tellg());
+        in.seekg(0, std::ios::beg);
+        in.read(&contents[0], contents.size());
+    }
     return 0;
 }
