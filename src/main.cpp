9ca9591a7f729e5dd8ec2986c041cc224b85c617
Chris Louie <deadpotat00@yahoo.com>
2013-12-05 00:13:12 -0800

    a stub parser.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include "precompile.h"
 
+#include <boost/fusion/include/adapt_struct.hpp>
 #include <iostream>
 #include <fstream>
 #include <string>
+
+template <typename Iterator>
+struct EcmaExecutor : public boost::spirit::qi::grammar<Iterator, unsigned()>
+{
+    public:
+        EcmaExecutor() 
+            : EcmaExecutor::base_type(program)
+        {
+            program = boost::spirit::qi::uint_;
+        }
+
+        boost::spirit::qi::rule<Iterator, unsigned()> program;
+};
 
 int main(int argc, char* argv[])
 {
@@ -14,6 +28,13 @@ int main(int argc, char* argv[])
         contents.resize(in.tellg());
         in.seekg(0, std::ios::beg);
         in.read(&contents[0], contents.size());
+
+        EcmaExecutor<std::string::const_iterator> executor;
+        unsigned program;
+        std::string::const_iterator start = contents.begin();
+        std::string::const_iterator end = contents.end();
+        boost::spirit::qi::parse(start, end, executor, program);
     }
+
     return 0;
 }
